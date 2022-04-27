75a44cc84c4518cc58448aec077479cdc9e18adb
Kelly Dwan <ryelle@users.noreply.github.com>
2022-04-27 16:08:04 -0400

    Reset child theme to minimum viable child theme

--- source/wp-content/themes/wporg-child-2021/functions.php
+++ source/wp-content/themes/wporg-child-2021/functions.php
@@ -11,6 +11,9 @@ add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets' );
  * Enqueue scripts and styles.
  */
 function enqueue_assets() {
+	// The parent style is registered as `wporg-parent-2021-style`, and will be loaded unless
+	// explicitly unregistered. We can load any child-theme overrides by declaring the parent
+	// stylesheet as a dependency.
 	wp_enqueue_style(
 		'wporg-child-2021-style',
 		get_stylesheet_uri(),
