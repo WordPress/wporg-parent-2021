a475d54427e2e406bd862bb9ac815b18ec241af2
Kelly Dwan <ryelle@users.noreply.github.com>
2022-04-28 16:12:10 -0400

    Add "Columns with border" pattern

---
+++ source/wp-content/themes/wporg-parent-2021/patterns/columns-with-border.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Title: Columns with Border
+ * Slug: wporg-parent-2021/columns-with-border
+ * Categories: columns
+ */
+
+?>
+<!-- wp:columns {"style":{"border":{"top":{"color":"var:preset|color|light-grey","width":"1px"}},"spacing":{"blockGap":"0px"}}} -->
+<div class="wp-block-columns" style="border-top-color:var(--wp--preset--color--light-grey);border-top-width:1px">
+	<!-- wp:column {"style":{"spacing":{"padding":{"top":"60px","right":"80px","bottom":"60px","left":"80px"}},"border":{"right":{"color":"var:preset|color|light-grey","width":"1px"}}}} -->
+	<div class="wp-block-column" style="border-right-color:var(--wp--preset--color--light-grey);border-right-width:1px;padding-top:60px;padding-right:80px;padding-bottom:60px;padding-left:80px">
+		<!-- wp:heading -->
+		<h2>Follow The Code</h2>
+		<!-- /wp:heading -->
+
+		<!-- wp:paragraph -->
+		<p>There’s <a href="https://make.wordpress.org/core/">a development blog</a> and you can track active development in the <a href="https://core.trac.wordpress.org/timeline">Trac timeline</a> that often has 20–30 updates per day.</p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:column -->
+
+	<!-- wp:column {"style":{"spacing":{"padding":{"top":"60px","right":"80px","bottom":"60px","left":"80px"}}}} -->
+	<div class="wp-block-column" style="padding-top:60px;padding-right:80px;padding-bottom:60px;padding-left:80px">
+		<!-- wp:heading -->
+		<h2>Find An Event Near You</h2>
+		<!-- /wp:heading -->
+
+		<!-- wp:paragraph -->
+		<p>Check out the <a href="https://central.wordcamp.org/">WordCamp schedule</a> and find your <a href="https://www.meetup.com/pro/wordpress">local Meetup group</a>! For more WordPress news, check out the <a href="http://planet.wordpress.org/">WordPress Planet</a>.</p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:column -->
+</div>
+<!-- /wp:columns -->
