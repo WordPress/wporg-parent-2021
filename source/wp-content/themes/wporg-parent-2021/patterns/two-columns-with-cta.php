6a27957d43fe24caa3be7af7b6fa6fd3c0a4f5ef
Adam Wood <adam.wood@automattic.com>
2022-07-28 15:25:19 +1200

    Add 2 column with CTA pattern
    Initial use is on downloads page

---
+++ source/wp-content/themes/wporg-parent-2021/patterns/two-columns-with-cta.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Title: 2 Columns with CTA
+ * Slug: wporg-parent-2021/two-columns-with-cta
+ * Categories: columns, buttons
+ */
+
+?>
+<!-- wp:columns {"align":"full","style":{"border":{"top":{"color":"var:preset|color|light-grey"}}},"className":"is-style-default"} -->
+<div class="wp-block-columns alignfull is-style-default" style="border-top-color:var(--wp--preset--color--light-grey)"><!-- wp:column {"width":"100%","style":{"spacing":{"padding":{"top":"80px","right":"100px","bottom":"100px","left":"100px"}}}} -->
+<div class="wp-block-column" style="padding-top:80px;padding-right:100px;padding-bottom:100px;padding-left:100px;flex-basis:100%"><!-- wp:heading {"level":4} -->
+<h4>Download and install yourself</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>For anyone comfortable getting their own hosting and domain.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"right":"32px","left":"32px"}}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" style="padding-right:32px;padding-left:32px">Download WordPress 6.0</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"textColor":"blue-1","style":{"spacing":{"padding":{"left":"32px","right":"32px"}}},"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-blue-1-color has-text-color wp-element-button" style="padding-right:32px;padding-left:32px">Installation guide</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:paragraph {"textColor":"dark-strokes-grey","fontSize":"tiny"} -->
+<p class="has-dark-strokes-grey-color has-text-color has-tiny-font-size">Recommend PHP 7.4 or greater and MySQL 5.7 or MariaDB version 10.3 or greater.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"100%","style":{"spacing":{"padding":{"top":"80px","right":"100px","bottom":"80px","left":"100px"}},"border":{"left":{"color":"var:preset|color|light-grey"}}}} -->
+<div class="wp-block-column" style="border-left-color:var(--wp--preset--color--light-grey);padding-top:80px;padding-right:100px;padding-bottom:80px;padding-left:100px;flex-basis:100%"><!-- wp:heading {"level":4} -->
+<h4>Set up with a hosting provider</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>For anyone looking for the simplest way to start.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"left":"32px","right":"32px"}}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" style="padding-right:32px;padding-left:32px">See all recommended hosts</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
