63b6ddea513b7ca04e02e2f6e5a663ab04e31b2f
ren <18050944+renintw@users.noreply.github.com>
2022-08-02 02:12:46 +0800

    Pattern: Add heading with arrow and list

---
+++ source/wp-content/themes/wporg-parent-2021/patterns/heading-with-arrow-and-list.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Title: Heading with arrow and list
+ * Slug: wporg-parent-2021/heading-with-arrow-and-list
+ * Categories: header
+ */
+
+?>
+<!-- wp:group {"align":"full","backgroundColor":"blueberry-1"} -->
+<div class="wp-block-group alignfull has-blueberry-1-background-color has-background">
+	<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"97px","bottom":"97px","right":"80px","left":"80px"}}},"className":"heading-with-arrow","layout":{"type":"flex","orientation":"vertical","justifyContent":"center","flexWrap":"nowrap"}} -->
+	<div class="wp-block-group alignfull heading-with-arrow" style="padding-top:97px;padding-right:80px;padding-bottom:97px;padding-left:80px">
+		<!-- wp:group {"style":{"spacing":{"blockGap":"0px"}},"layout":{"inherit":false}} -->
+		<div class="wp-block-group">
+			<!-- wp:group {"style":{"spacing":{"blockGap":"65px","padding":{"bottom":"67px"}}},"className":"heading-with-arrow__row","layout":{"type":"flex","flexWrap":"nowrap","orientation":"horizontal","justifyContent":"center"}} -->
+			<div class="wp-block-group heading-with-arrow__row" style="padding-bottom:67px">
+				<!-- wp:group {"layout":{"contentSize":"610px"}} -->
+				<div class="wp-block-group">
+					<!-- wp:paragraph {"style":{"typography":{"fontSize":"120px","fontStyle":"normal","fontWeight":"300","lineHeight":"1.1","letterSpacing":"-0.02em"}},"textColor":"white"} -->
+					<p class="has-white-color has-text-color" style="font-size:120px;font-style:normal;font-weight:300;letter-spacing:-0.02em;line-height:1.1">Get started</p>
+					<!-- /wp:paragraph -->
+				</div>
+				<!-- /wp:group -->
+
+				<!-- wp:image {"id":8960,"width":479,"height":120,"sizeSlug":"full","linkDestination":"none","style":{"spacing":{"margin":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}}}} -->
+				<figure class="wp-block-image size-full is-resized" style="margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px"><img src="https://wordpress.org/main-test/files/2022/07/Arrow-3.png" alt="" class="wp-image-8960" width="479" height="120"/></figure>
+				<!-- /wp:image -->
+			</div>
+			<!-- /wp:group -->
+
+			<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+			<div class="wp-block-group">
+				<!-- wp:list {"ordered":true,"style":{"typography":{"fontStyle":"normal","fontWeight":"300","lineHeight":"1.7","letterSpacing":"0.7px"}},"className":"has-white-color has-text-color heading-with-arrow__list","fontSize":"normal","fontFamily":"inter"} -->
+				<ol class="has-white-color has-text-color heading-with-arrow__list has-inter-font-family has-normal-font-size" style="font-style:normal;font-weight:300;letter-spacing:0.7px;line-height:1.7">
+					<li>Find a trusted <a href="http://w.org" data-type="URL" data-id="w.org">web host</a>.</li>
+					<li><a href="http://w.org">Download</a> &amp; <a href="http://w.org">Install</a> WordPress (in just 5 minutes). </li>
+					<li>Spend some time <a href="http://wp.org">reading our documentation</a>.</li>
+				</ol>
+				<!-- /wp:list -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
