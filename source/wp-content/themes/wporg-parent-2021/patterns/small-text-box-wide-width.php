10695704b729e8f736b81444619ecf1ecc85ee58
ren <18050944+renintw@users.noreply.github.com>
2022-08-03 02:56:16 +0800

    Pattern: Small text box wide width

---
+++ source/wp-content/themes/wporg-parent-2021/patterns/small-text-box-wide-width.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: Small text box - wide width
+ * Slug: wporg-parent-2021/small-text-box-wide-width
+ * Categories: text, header
+ */
+
+?>
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"80px","bottom":"80px"}}},"backgroundColor":"blueberry-4","layout":{"type":"default","contentSize":"895px"}} -->
+<div class="wp-block-group alignwide has-blueberry-4-background-color has-background" style="padding-top:80px;padding-bottom:80px">
+	<!-- wp:paragraph {"align":"center"} -->
+	<p class="has-text-align-center">If you do decide to go with one of the hosts above and click through from this page, some will donate a portion of your fee back — so you can have a great host and support WordPress.org at the same time. If you'd like to try WordPress for free, you can get started with a free website or blog at <a href="https://wordpress.com/">WordPress.com</a>.</p>
+	<!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
