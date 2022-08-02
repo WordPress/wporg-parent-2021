75b2341a8f33471d335c4aba60baba02daa7b621
ren <18050944+renintw@users.noreply.github.com>
2022-08-03 01:01:03 +0800

    Pattern: Add Hero

---
+++ source/wp-content/themes/wporg-parent-2021/patterns/hero.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Title: Hero
+ * Slug: wporg-parent-2021/hero
+ * Categories: header
+ */
+
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"120px","right":"344px","bottom":"256px","left":"80px"}}},"layout":{"contentSize":"896px"}} -->
+<div class="wp-block-group alignfull" style="padding-top:120px;padding-right:344px;padding-bottom:256px;padding-left:80px">
+	<!-- wp:group {"style":{"spacing":{"blockGap":"30px"}},"className":"hero","layout":{"type":"flex","orientation":"vertical","justifyContent":"left"}} -->
+	<div class="wp-block-group hero">
+		<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400","lineHeight":"1","letterSpacing":"-0.02em"}},"textColor":"charcoal-1","className":"hero__title","fontSize":"heading-cta"} -->
+		<p class="hero__title has-charcoal-1-color has-text-color has-heading-cta-font-size" style="font-style:normal;font-weight:400;letter-spacing:-0.02em;line-height:1">WordPress, your tool to <em><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-blueberry-1-color">share</mark></em></p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:group {"layout":{"contentSize":"310px"}} -->
+		<div class="wp-block-group">
+			<!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.6"}},"textColor":"charcoal-1","className":"hero__subtitle","fontSize":"normal"} -->
+			<p class="hero__subtitle has-charcoal-1-color has-text-color has-normal-font-size" style="line-height:1.6">Free, open source software you can use to create a beautiful website, blog, or app.</p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:group -->
+
+		<!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.6"}},"textColor":"charcoal-1","className":"hero__subtitle","fontSize":"extra-small"} -->
+		<p class="hero__subtitle has-charcoal-1-color has-text-color has-extra-small-font-size" style="line-height:1.6">Feeling confused? Learn the <a href="http://wp.org">difference between</a> WordPress.org and WordPress.com.</p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
