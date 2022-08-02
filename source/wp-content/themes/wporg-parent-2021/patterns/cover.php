2f661a0795d9809fc0d8497ab0bf87c83ff79b9a
ren <18050944+renintw@users.noreply.github.com>
2022-08-03 01:43:34 +0800

    Pattern: Add Cover

---
+++ source/wp-content/themes/wporg-parent-2021/patterns/cover.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Title: Cover
+ * Slug: wporg-parent-2021/cover
+ * Categories: text, buttons, header
+ */
+
+?>
+<!-- wp:group {"align":"full","style":{"color":{"background":"#f5f5f5"},"spacing":{"padding":{"top":"300px","bottom":"100px","left":"100px"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group alignfull has-background" style="background-color:#f5f5f5;padding-top:300px;padding-bottom:100px;padding-left:100px">
+	<!-- wp:group {"style":{"spacing":{"blockGap":"30px"}},"className":"hero","layout":{"type":"flex","orientation":"vertical","justifyContent":"left"}} -->
+	<div class="wp-block-group hero">
+		<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400","lineHeight":"1","letterSpacing":"0.01em"}},"textColor":"charcoal-1","fontSize":"heading-2"} -->
+		<p class="has-charcoal-1-color has-text-color has-heading-2-font-size" style="font-style:normal;font-weight:400;letter-spacing:0.01em;line-height:1">Built by a community</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:group {"layout":{"contentSize":"370px"}} -->
+		<div class="wp-block-group">
+			<!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.6","letterSpacing":"0.05em"}},"textColor":"charcoal-1","fontSize":"normal"} -->
+			<p class="has-charcoal-1-color has-text-color has-normal-font-size" style="letter-spacing:0.05em;line-height:1.6">Hundreds of thousands of developers, content creators, and site owners gather at monthly meetups in 817 cities worldwide.</p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:group -->
+
+		<!-- wp:buttons -->
+		<div class="wp-block-buttons">
+			<!-- wp:button -->
+			<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="wp.org">Find a local WordPress community</a></div>
+			<!-- /wp:button -->
+		</div>
+		<!-- /wp:buttons -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
