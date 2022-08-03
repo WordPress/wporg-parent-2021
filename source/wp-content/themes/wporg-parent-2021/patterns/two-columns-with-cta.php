5f3495897de26ae2ccdfbe7fb6244a984ddba0c1
Adam Wood <adam.wood@automattic.com>
2022-08-03 12:03:01 +1200

    Update pattern and associated styles

--- source/wp-content/themes/wporg-parent-2021/patterns/two-columns-with-cta.php
+++ source/wp-content/themes/wporg-parent-2021/patterns/two-columns-with-cta.php
@@ -6,44 +6,64 @@
  */
 
 ?>
-<!-- wp:columns {"align":"full","style":{"border":{"top":{"color":"var:preset|color|light-grey"}}},"className":"is-style-default"} -->
-<div class="wp-block-columns alignfull is-style-default" style="border-top-color:var(--wp--preset--color--light-grey)"><!-- wp:column {"width":"100%","style":{"spacing":{"padding":{"top":"80px","right":"100px","bottom":"100px","left":"100px"}}}} -->
-<div class="wp-block-column" style="padding-top:80px;padding-right:100px;padding-bottom:100px;padding-left:100px;flex-basis:100%"><!-- wp:heading {"level":4} -->
-<h4>Download and install yourself</h4>
-<!-- /wp:heading -->
 
-<!-- wp:paragraph -->
-<p>For anyone comfortable getting their own hosting and domain.</p>
-<!-- /wp:paragraph -->
+<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":"0px"}},"className":"wporg-pattern__two-columns-cta"} -->
+<div class="wp-block-columns alignwide wporg-pattern__two-columns-cta">
 
-<!-- wp:buttons -->
-<div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"right":"32px","left":"32px"}}}} -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" style="padding-right:32px;padding-left:32px">Download WordPress 6.0</a></div>
-<!-- /wp:button -->
+	<!-- wp:column {"width":"50%","style":{"border":{"right":{"color":"var:preset|color|light-grey-1","width":"1px"}}}} -->
+	<div class="wp-block-column" style="border-right-color:var(--wp--preset--color--light-grey-1);border-right-width:1px;flex-basis:50%">
+		
+	<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|60","bottom":"var:preset|spacing|50","left":"var:preset|spacing|60"}}}} -->
+		<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--60)">
+			
+		<!-- wp:heading {"textAlign":"left","fontSize":"huge"} -->
+			<h2 class="has-text-align-left has-huge-font-size">Download and install yourself</h2>
+			<!-- /wp:heading -->
 
-<!-- wp:button {"textColor":"blue-1","style":{"spacing":{"padding":{"left":"32px","right":"32px"}}},"className":"is-style-outline"} -->
-<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-blue-1-color has-text-color wp-element-button" style="padding-right:32px;padding-left:32px">Installation guide</a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons -->
+			<!-- wp:paragraph {"align":"left"} -->
+			<p class="has-text-align-left">For anyone comfortable getting their own hosting and domain.</p>
+			<!-- /wp:paragraph -->
 
-<!-- wp:paragraph {"textColor":"dark-strokes-grey","fontSize":"tiny"} -->
-<p class="has-dark-strokes-grey-color has-text-color has-tiny-font-size">Recommend PHP 7.4 or greater and MySQL 5.7 or MariaDB version 10.3 or greater.</p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:column -->
+			<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left"},"style":{"spacing":{"blockGap":"10px"}}} -->
+			<div class="wp-block-buttons">
+				<!-- wp:button -->
+				<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="https://wordpress.org/latest.zip">Download WordPress 6.0.1</a></div>
+				<!-- /wp:button -->
 
-<!-- wp:column {"width":"100%","style":{"spacing":{"padding":{"top":"80px","right":"100px","bottom":"80px","left":"100px"}},"border":{"left":{"color":"var:preset|color|light-grey"}}}} -->
-<div class="wp-block-column" style="border-left-color:var(--wp--preset--color--light-grey);padding-top:80px;padding-right:100px;padding-bottom:80px;padding-left:100px;flex-basis:100%"><!-- wp:heading {"level":4} -->
-<h4>Set up with a hosting provider</h4>
-<!-- /wp:heading -->
+				<!-- wp:button {"textColor":"blue-1","className":"is-style-outline"} -->
+				<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-blue-1-color has-text-color wp-element-button" href="https://wordpress.org/latest.tar.gz">Installation guide</a></div>
+				<!-- /wp:button --></div>
+			<!-- /wp:buttons -->
 
-<!-- wp:paragraph -->
-<p>For anyone looking for the simplest way to start.</p>
-<!-- /wp:paragraph -->
+			<!-- wp:paragraph {"textColor":"charcoal-4","fontSize":"extra-small"} -->
+			<p class="has-charcoal-4-color has-text-color has-extra-small-font-size">Recommend PHP 7.4 or greater and MySQL 5.7 or MariaDB version 10.3 or greater.</p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:column -->
 
-<!-- wp:buttons -->
-<div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"left":"32px","right":"32px"}}}} -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" style="padding-right:32px;padding-left:32px">See all recommended hosts</a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons --></div>
-<!-- /wp:column --></div>
+	<!-- wp:column {"width":"50%"} -->
+	<div class="wp-block-column" style="flex-basis:50%">
+		<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","right":"var:preset|spacing|60","bottom":"var:preset|spacing|50","left":"var:preset|spacing|60"}}}} -->
+		<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--60)">
+			<!-- wp:heading {"textAlign":"left","fontSize":"huge"} -->
+			<h2 class="has-text-align-left has-huge-font-size">Set up with a hosting provider</h2>
+			<!-- /wp:heading -->
+
+			<!-- wp:paragraph {"align":"left"} -->
+			<p class="has-text-align-left">For anyone looking for the simplest way to start.</p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:buttons -->
+			<div class="wp-block-buttons">
+				<!-- wp:button -->
+				<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="https://wordpress.org/main-test/hosting/">See all recommended hosts</a></div>
+				<!-- /wp:button --></div>
+			<!-- /wp:buttons -->
+		</div>
+		<!-- /wp:group -->
+	</div>
+	<!-- /wp:column -->
+</div>
 <!-- /wp:columns -->
