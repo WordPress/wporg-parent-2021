49c061aa4c771a29a511f1a9f304abf0c9fc510b
Kelly Dwan <ryelle@users.noreply.github.com>
2022-08-04 18:29:22 -0400

    Set the font size on the heading, using the CTA preset

--- source/wp-content/themes/wporg-parent-2021/patterns/heading-with-arrow.php
+++ source/wp-content/themes/wporg-parent-2021/patterns/heading-with-arrow.php
@@ -10,7 +10,7 @@
 <div class="wp-block-group alignfull has-blueberry-1-background-color has-background">
 	<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"97px","bottom":"97px"},"blockGap":"0px"}},"className":"wporg-pattern__heading-with-arrow"} -->
 	<div class="wp-block-group alignfull wporg-pattern__heading-with-arrow has-link-color" style="padding-top:97px;padding-bottom:97px">
-		<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"300","lineHeight":"1.1","letterSpacing":"-0.02em"}},"fontFamily":"inter"} -->
+		<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"300","lineHeight":"1.1","letterSpacing":"-0.02em"}},"fontSize":"heading-cta","fontFamily":"inter"} -->
 		<h2 class="has-inter-font-family" style="font-style:normal;font-weight:300;letter-spacing:-0.02em;line-height:1.1"><a href="https://wordpress.org/download/">Get started</a></h2>
 		<!-- /wp:heading -->
 	</div>
