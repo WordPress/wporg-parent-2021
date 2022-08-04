65675c381e55b6f8c906aa11ebd8bfd859a29c1a
ren <18050944+renintw@users.noreply.github.com>
2022-08-05 03:58:48 +0800

    Pattern: Add heading with arrow

---
+++ source/wp-content/themes/wporg-parent-2021/patterns/heading-with-arrow.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Title: Heading with arrow
+ * Slug: wporg-parent-2021/heading-with-arrow
+ * Categories: header
+ */
+
+?>
+<!-- wp:group {"align":"full","backgroundColor":"blueberry-1"} -->
+<div class="wp-block-group alignfull has-blueberry-1-background-color has-background">
+	<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"97px","bottom":"97px"},"blockGap":"0px"}},"className":"wporg-pattern__heading-with-arrow"} -->
+	<div class="wp-block-group alignfull wporg-pattern__heading-with-arrow has-link-color" style="padding-top:97px;padding-bottom:97px">
+		<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"300","lineHeight":"1.1","letterSpacing":"-0.02em"}},"fontFamily":"inter"} -->
+		<h2 class="has-inter-font-family" style="font-style:normal;font-weight:300;letter-spacing:-0.02em;line-height:1.1"><a href="https://wordpress.org/download/">Get started</a></h2>
+		<!-- /wp:heading -->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
