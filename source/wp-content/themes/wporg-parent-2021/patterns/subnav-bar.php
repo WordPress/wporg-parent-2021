ba8795061a86560bc677f52158bf6a90f862920a
Kelly Dwan <ryelle@users.noreply.github.com>
2022-10-20 16:34:12 -0400

    Patterns: Add a new pattern for the subpage header navigation

---
+++ source/wp-content/themes/wporg-parent-2021/patterns/subnav-bar.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: Subpage Header Bar (Brush Stroke)
+ * Slug: wporg-parent-2021/subnav-bar
+ * Categories: wporg
+ */
+
+?>
+<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"16px","right":"var:preset|spacing|20","bottom":"0","left":"var:preset|spacing|20"}}},"backgroundColor":"blueberry-1","textColor":"white","className":"is-style-brush-stroke","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-style-brush-stroke has-white-color has-blueberry-1-background-color has-text-color has-background has-link-color" style="padding-top:16px;padding-right:var(--wp--preset--spacing--20);padding-bottom:0;padding-left:var(--wp--preset--spacing--20)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide"><!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"normal"} -->
+<p class="has-normal-font-size" style="font-style:normal;font-weight:700"><?php esc_html_e( 'Section Title', 'wporg' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:navigation {"ref":null,"backgroundColor":"blueberry-1","className":"is-style-dropdown-on-mobile","style":{"spacing":{"blockGap":"var:preset|spacing|30"}}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
