90ea88f7818162574465f2e793f88a536f615fe0
ren <18050944+renintw@users.noreply.github.com>
2022-11-17 19:35:40 +0800

    Add 404 template & pattern

---
+++ source/wp-content/themes/wporg-parent-2021/patterns/404-page.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Title: 404 Page
+ * Slug: wporg-parent-2021/404-page
+ * Categories: wporg
+ */
+
+?>
+
+<!-- wp:group {"className":"site-content-container"} -->
+<div class="wp-block-group site-content-container">
+	<p class="error404__oops" aria-hidden="true">
+		<?php esc_html_e( 'Oops!', 'wporg' ); ?>
+	</p>
+	
+	<!-- wp:heading {"level":1} -->
+	<h1><?php esc_html_e( 'This page doesn\'t exist.', 'wporg' ); ?></h1>
+	<!-- /wp:heading -->
+	
+	<!-- wp:paragraph -->
+	<p>
+		<?php esc_html_e( 'Go to', 'wporg' ); ?>
+		<a href="https://wordpress.org"><?php esc_html_e( 'the homepage', 'wporg' ); ?></a>,
+		<?php esc_html_e( 'or try searching WordPress.org sites using the field below.', 'wporg' ); ?>
+	</p>
+	<!-- /wp:paragraph -->
+
+	<!-- wp:search {"label":"","placeholder":"Search...","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true} /-->
+</div>
+<!-- /wp:group -->
+
