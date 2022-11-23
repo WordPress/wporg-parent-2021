58b7bcee64dbc0496539759438df4b4cb7e7549c
ren <18050944+renintw@users.noreply.github.com>
2022-11-23 17:18:21 +0800

    Fix tablet layout

--- source/wp-content/themes/wporg-parent-2021/patterns/404-page.php
+++ source/wp-content/themes/wporg-parent-2021/patterns/404-page.php
@@ -8,17 +8,17 @@
 ?>
 
 <!-- wp:group {"style":{"border":{"top":{"width":"1px","color":"var:preset|color|charcoal-1"},"right":{"width":"0px","style":"none"},"bottom":{"width":"0px","style":"none"},"left":{"width":"0px","style":"none"}},"spacing":{"padding":{"left":"var:preset|spacing|60","right":"var:preset|spacing|60"},"margin":{"top":"0"}}},"backgroundColor":"charcoal-2","textColor":"white","className":"site-content-container"} -->
-<div class="wp-block-group site-content-container has-white-color has-charcoal-2-background-color has-text-color has-background" style="border-top-color:var(--wp--preset--color--charcoal-1);border-top-width:1px;border-right-style:none;border-right-width:0px;border-bottom-style:none;border-bottom-width:0px;border-left-style:none;border-left-width:0px;margin-top:0;padding-right:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:group {"textColor":"charcoal-1","className":"wporg-parent-oops-container","layout":{"type":"constrained"}} -->
+<main class="wp-block-group site-content-container has-white-color has-charcoal-2-background-color has-text-color has-background" style="border-top-color:var(--wp--preset--color--charcoal-1);border-top-width:1px;border-right-style:none;border-right-width:0px;border-bottom-style:none;border-bottom-width:0px;border-left-style:none;border-left-width:0px;margin-top:0;padding-right:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:group {"textColor":"charcoal-1","className":"wporg-parent-oops-container","layout":{"type":"constrained"}} -->
 <div class="wp-block-group wporg-parent-oops-container has-charcoal-1-color has-text-color"><!-- wp:paragraph {"textColor":"white"} -->
 <p class="has-white-color has-text-color">Change the text color of the group here to adjust "Oops" color.</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
-<!-- wp:heading {"level":1,"fontSize":"heading-3"} -->
-<h1 class="has-heading-3-font-size"><?php esc_html_e( 'This page doesn\'t exist.', 'wporg' ); ?></h1>
+<!-- wp:heading {"level":1} -->
+<h1><?php esc_html_e( 'This page doesn\'t exist.', 'wporg' ); ?></h1>
 <!-- /wp:heading -->
 
 <!-- wp:pattern {"slug":"wporg-parent-2021/404-page-subtitle"} /-->
 
-<!-- wp:search {"showLabel":false,"placeholder":"Search...","width":100,"widthUnit":"%","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true,"style":{"color":{"background":"#ffffff00"}},"textColor":"charcoal-1","className":"is-style-default"} /--></div>
+<!-- wp:search {"showLabel":false,"placeholder":"Search...","width":100,"widthUnit":"%","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true,"style":{"color":{"background":"#ffffff00"}},"textColor":"charcoal-1","className":"is-style-default"} /--></main>
 <!-- /wp:group -->
