ee74834e7bdcc736bf6f0dbd65df1c3bb722f605
Ren <18050944+renintw@users.noreply.github.com>
2022-11-30 01:45:26 +0800

    Add 404 page (#54)
    * Add 404 template & pattern
    
    * Add 404 stylesheet
    
    * Render the text Oops with CSS
    
    * Update styles for matching the 404 page of the News
    
    * Switch the 404-page header to the global header
    
    * Make styles controllable via block editor as much as possible 
    
    * Do not show secondary content in the footer
    
    * Fix tablet layout
    
    * Update subtitle text
    
    * Replace straight curly quote with curly quote

---
+++ source/wp-content/themes/wporg-parent-2021/patterns/404-page-subtitle.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Title: 404 Page Subtitle
+ * Slug: wporg-parent-2021/404-page-subtitle
+ * Inserter: no
+ */
+
+?>
+
+<!-- wp:paragraph -->
+<p>
+	<?php
+	printf(
+		/* translators: %s is the site URL. */
+		wp_kses_post( __( 'Go to <a href="%s">the homepage</a> or try searching using the field below.', 'wporg' ) ),
+		esc_url( get_site_url( '/' ) )
+	);
+	?>
+</p>
+<!-- /wp:paragraph -->
---
+++ source/wp-content/themes/wporg-parent-2021/patterns/404-page.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Title: 404 Page
+ * Slug: wporg-parent-2021/404-page
+ * Inserter: no
+ */
+
+?>
+
+<!-- wp:group {"style":{"border":{"top":{"width":"1px","color":"var:preset|color|charcoal-1"},"right":{"width":"0px","style":"none"},"bottom":{"width":"0px","style":"none"},"left":{"width":"0px","style":"none"}},"spacing":{"padding":{"left":"var:preset|spacing|60","right":"var:preset|spacing|60"},"margin":{"top":"0"}}},"backgroundColor":"charcoal-2","textColor":"white","className":"site-content-container"} -->
+<main class="wp-block-group site-content-container has-white-color has-charcoal-2-background-color has-text-color has-background" style="border-top-color:var(--wp--preset--color--charcoal-1);border-top-width:1px;border-right-style:none;border-right-width:0px;border-bottom-style:none;border-bottom-width:0px;border-left-style:none;border-left-width:0px;margin-top:0;padding-right:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:group {"textColor":"charcoal-1","className":"wporg-parent-oops-container","layout":{"type":"constrained"}} -->
+<div class="wp-block-group wporg-parent-oops-container has-charcoal-1-color has-text-color"><!-- wp:paragraph {"textColor":"white"} -->
+<p class="has-white-color has-text-color">Change the text color of the group here to adjust "Oops" color.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:heading {"level":1} -->
+<h1><?php esc_html_e( 'This page doesn&#146;t exist.', 'wporg' ); ?></h1>
+<!-- /wp:heading -->
+
+<!-- wp:pattern {"slug":"wporg-parent-2021/404-page-subtitle"} /-->
+
+<!-- wp:search {"showLabel":false,"placeholder":"Search...","width":100,"widthUnit":"%","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true,"style":{"color":{"background":"#ffffff00"}},"textColor":"charcoal-1","className":"is-style-default"} /--></main>
+<!-- /wp:group -->
