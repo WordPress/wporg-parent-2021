1bb4007d3bf8b5549219e30846f25ef9f460f168
Kelly Dwan <ryelle@users.noreply.github.com>
2023-06-14 18:46:43 -0400

    Use the post-navigation pattern

--- source/wp-content/themes/wporg-parent-2021/patterns/post-navigation.php
+++ source/wp-content/themes/wporg-parent-2021/patterns/post-navigation.php
@@ -6,9 +6,8 @@
  */
 
 ?>
-
-<!-- wp:group {"style":{"border":{"top":{"color":"var:preset|color|light-grey-1","width":"1px"}},"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-<div class="wp-block-group" style="border-top-color:var(--wp--preset--color--light-grey-1);border-top-width:1px;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)">
+<!-- wp:group {"style":{"border":{"top":{"color":"var:preset|color|light-grey-1","width":"1px"}},"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|edge-space","bottom":"var:preset|spacing|20","left":"var:preset|spacing|edge-space"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group" style="border-top-color:var(--wp--preset--color--light-grey-1);border-top-width:1px;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--edge-space)">
 	<!-- wp:post-navigation-link {"type":"previous","showTitle":true,"label":"<?php esc_attr_e( 'Previous', 'wporg' ); ?>","linkLabel":true} /-->
 	<!-- wp:post-navigation-link {"type":"next","showTitle":true,"label":"<?php esc_attr_e( 'Next', 'wporg' ); ?>","linkLabel":true} /-->
 </div>
