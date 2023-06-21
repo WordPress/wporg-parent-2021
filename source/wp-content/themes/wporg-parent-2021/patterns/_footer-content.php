ee7faff38cb2ffebdd6a52b3c4d6d26a332f03f0
Kelly Dwan <ryelle@users.noreply.github.com>
2023-06-21 13:31:30 -0400

    Templates: Update single and archive templates (#88)
    * Use the post-navigation pattern
    
    * Move the footer content into a pattern for translation
    
    * Update spacing
    
    * Use the same layout on archive and index pages
    
    This also introduces a filter on query-title to output a heading on non-archive pages.

---
+++ source/wp-content/themes/wporg-parent-2021/patterns/_footer-content.php
@@ -0,0 +1,28 @@
+<?php
+// phpcs:disable WordPress.Files.FileName -- Allow underscore for pattern partial.
+/**
+ * Title: Footer content
+ * Slug: wporg-parent-2021/footer-content
+ * Inserter: no
+ */
+
+?>
+<!-- wp:columns {"style":{"border":{"top":{"color":"var:preset|color|light-grey-1","width":"1px"}},"spacing":{"blockGap":"0px"}}} -->
+<div class="wp-block-columns" style="border-top-color:var(--wp--preset--color--light-grey-1);border-top-width:1px"><!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|edge-space","bottom":"var:preset|spacing|30","left":"var:preset|spacing|edge-space"}},"border":{"right":{"color":"var:preset|color|light-grey-1","width":"1px"}}}} -->
+<div class="wp-block-column" style="border-right-color:var(--wp--preset--color--light-grey-1);border-right-width:1px;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--edge-space)">
+<!-- wp:heading {"style":{"spacing":{"margin":{"top":"0"}}}} -->
+<h2 class="wp-block-heading" style="margin-top:0"><?php esc_html_e( 'Categories', 'wporg' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:categories {"showHierarchy":true,"showPostCounts":true} /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|edge-space","right":"var:preset|spacing|edge-space"}}}} -->
+<div class="wp-block-column" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--edge-space)">
+<!-- wp:heading {"style":{"spacing":{"margin":{"top":"0"}}}} -->
+<h2 class="wp-block-heading" style="margin-top:0"><?php esc_html_e( 'Subscribe', 'wporg' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:jetpack/subscriptions {"borderColor":"blueberry-1","className":"is-style-compact"} /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
