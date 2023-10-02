e77396168c0efb33ecc1196d5b9fd96874a627ed
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2023-10-02 14:09:06 +1300

    Add style variations to editor

--- source/wp-content/themes/wporg-parent-2021/inc/block-styles.php
+++ source/wp-content/themes/wporg-parent-2021/inc/block-styles.php
@@ -79,6 +79,30 @@ function setup_block_styles() {
 	);
 
 	register_block_style(
+		'core/button',
+		array(
+			'name'         => 'small',
+			'label'        => __( 'Default Small', 'wporg' ),
+		)
+	);
+
+	register_block_style(
+		'core/button',
+		array(
+			'name'         => 'tertiary',
+			'label'        => __( 'Tertiary', 'wporg' ),
+		)
+	);
+
+	register_block_style(
+		'core/button',
+		array(
+			'name'         => 'toggle',
+			'label'        => __( 'Toggle', 'wporg' ),
+		)
+	);
+
+	register_block_style(
 		'core/list',
 		array(
 			'name'         => 'features',
